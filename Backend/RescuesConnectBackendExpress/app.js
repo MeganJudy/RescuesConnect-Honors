fa194a6195f800aeb3503fc069d18a012a8fbff7
Megan Judy <meganrosejudy1@gmail.com>
2022-03-24 22:42:07 -0400

    Auth Pt 1

--- Backend/RescuesConnectBackendExpress/app.js
+++ Backend/RescuesConnectBackendExpress/app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 const url = 'mongodb://localhost:27017/rcmongodb';
 const connect = mongoose.connect(url, {
@@ -31,13 +33,41 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(cookieParser("i-love-dogs"));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/dogs', dogSearchRouter);
-app.use('/partners', partnerRouter);
+
+function auth(req, res, next) {
+  console.log(req.session);
+
+  if (!req.session.user) {
+    const err = new Error('You are not authenticated!');
+    err.status = 401;
+    return next(err);
+  } else {
+    if (req.session.user === 'authenticated') {
+      return next();
+    } else {
+      const err = new Error('You are not authenticated!');
+      err.status = 401;
+      return next(err);
+    }
+  }
+}
+
+app.use(auth);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
